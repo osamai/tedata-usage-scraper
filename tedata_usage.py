85032777105f4ad037316d239e43992bb2962e05
Osama <iosamaify@gmail.com>
2021-03-03 14:25:02 +0200

    get number and password from arguments or env vars

--- tedata_usage.py
+++ tedata_usage.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -6,12 +8,12 @@ from selenium.webdriver.support.expected_conditions import presence_of_element_l
 
 
 DOMAIN = "https://my.te.eg"
-SERVICE_NUMBER = ""
-SERVICE_PASSWORD = ""
+SERVICE_NUMBER = sys.argv[1] if len(sys.argv) >= 2 else os.getenv("TEDATA_SERVICE_NUMBER")
+SERVICE_PASSWORD = sys.argv[2] if len(sys.argv) >= 3 else os.getenv("TEDATA_SERVICE_PASSWORD")
 
 with webdriver.Firefox() as driver:
     wait = WebDriverWait(driver, 2)
-    driver.get(DOMAIN+"/#/home/signin")
+    driver.get(f"{DOMAIN}/#/home/signin")
     driver.find_element_by_id("MobileNumberID").send_keys(SERVICE_NUMBER)
     driver.find_element_by_id("PasswordID").send_keys(SERVICE_PASSWORD+Keys.RETURN)
     #driver.find_element_by_id("singInBtn").click()
